43b952ae50ec5b8acd40320de195614743da95c7
Aviv Tal <76473972+avivt1205@users.noreply.github.com>
2024-04-04 21:02:30 +0300

    adding components and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "../src/styles/index.css";
-import Layout from "./layout/Layout";
+import '../src/styles/tipography.css';
+import styles from '../src/styles/Main.module.css'
+import Hero from '../src/components/Hero'
+import About from '../src/components/About'
+import Systems from '../src/components/Systems'
+import Contact from '../src/components/Contact'
 function App() {
 
   return (
-    <>
-      <main className="site-container">
-          <Layout/>
+      <main className='site-container'>
+        <div className={styles.main_layout}>
+          <Hero />
+          <About />
+          <Systems />
+          <Contact />
+        </div>
       </main>
-    </>
   )
 }
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import '../styles/tipography.css';
+import styles from '../styles/About.module.css';
+
+import { aboutEducation, aboutProfessions, aboutExternalCourses} from '../content/SiteData';
 
 const About = () => {
   return (
-    <section id='about'>
-
+    <section id='about' className='seperator'>
+      <div className={styles.about_wrapper}>
+        <h2 className='headline'>קצת עלי</h2>
+        <div className={styles.cubes_wrapper}>
+          <div className={styles.cube}>
+            <img src='../../../../../' className={styles.cube_icon}/>
+            <h4 className='subheadline flex gap_block'>
+              ניסיון מקצועי
+            </h4>
+            <ul className='cube_content_wrapper'>
+              {aboutProfessions.map((profession) => {
+                return (
+                  <li className='cube_content'>
+                    <h5 className='cube_content_title'>{profession.title}</h5>                   
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className={styles.cube}>
+            <img src='../../../../../' className={styles.cube_icon}/>
+            <h4 className='subheadline flex gap_block'>
+              תארים אקדמיים וקורסי השלמה
+            </h4>
+            <ul className='cube_content_wrapper'>
+              {aboutEducation.map((education) => {
+                return (
+                  <li className='cube_content'>
+                    <h5 className='cube_content_title'>{education.title}</h5>                   
+                  </li>
+                )
+              })}
+            </ul>
+            <h5 className='intro_title gap_block'>הכשרות וקורסים נוספים</h5>
+            <ul className='cube_content_wrapper'>
+            {aboutExternalCourses.map((course) => {
+                return (
+                  <li className='cube_content'>
+                    <h5 className='cube_content_title'>{course.title}</h5>                   
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
